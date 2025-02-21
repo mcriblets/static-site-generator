fbc2447403cc9b83e4ba62dd0d3c04dc73e37dd6
mcriblets <mrobinson3@gatech.edu>
2025-02-21 16:14:57 -0500

    adds leafnode logic

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -19,3 +19,19 @@ class HTMLNode():
     def __repr__(self):
         html_node = self.__class__.__name__ 
         return f"HTMLNode({self.tag,}, {self.value}, {self.children}, {self.props})"
+    
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+        
+    def to_html(self):
+        leaf_string = ""
+        if not self.value:
+            raise ValueError("All Leaf Nodes must have a value.")
+        elif not self.tag:
+            return str(self.value)
+        else:
+            leaf_string = f'<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>'
+        
+        return leaf_string
