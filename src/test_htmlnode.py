73bd85eb0c692ede9a7b122082e7be6fbb482498
mcriblets <mrobinson3@gatech.edu>
2025-02-21 16:30:27 -0500

    adds leafnode unit tests

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -48,4 +48,32 @@ class TestHTMLNode(unittest.TestCase):
         self.assertEqual(node.props_to_html(), props_string)
         self.assertNotEqual(node.props_to_html(), incorrect_props_string)
         
+        
+class TestLeafNode(unittest.TestCase):
+    
+    def test(self):
+        node = LeafNode("p", "This is a paragraph of text.")
+        render_string = "<p>This is a paragraph of text.</p>"
+        
+        self.assertEqual(node.to_html(), render_string)
             
+            
+    def test2(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        render_string = '<a href="https://www.google.com">Click me!</a>'
+        
+        self.assertEqual(node.to_html(), render_string)
+        
+    
+    def test3(self):
+        node = LeafNode("a", "General Kenobi!", {"href": "https://www.well-hello-there.com"})
+        render_string = '<a href="https://www.well-hello-there.com">General Kenobi!</a>'
+        
+        self.assertEqual(node.to_html(), render_string)
+        
+    
+    def test4(self):
+        node = LeafNode("b", "Kabal Wins!")
+        render_string = '<p>Kabal Wins1</p>'
+        
+        self.assertNotEqual(node.to_html(), render_string)
