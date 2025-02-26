34e259e1dab198296aa6148c80e9674c73436dec
mcriblets <mrobinson3@gatech.edu>
2025-02-26 14:41:57 -0500

    both recursive and easy copy functions

--- src/main.py
+++ src/main.py
@@ -1,21 +1,45 @@
 import re
+import os
+import shutil
 from textnode import *
 from htmlnode import *
 from splitnodes import *
 
 
-def main():
-    markdown = """# This is a heading
-
-This is a paragraph of text. It has some **bold** and *italic* words inside of it.
-
-* This is the first list item in a list block
-* This is a list item
-* This is another list item
-    """
+def copy_contents(source_directory, target_directory): 
     
-    print(markdown)
-    markdown_split = markdown_to_blocks(markdown)
+    source_path = os.path.abspath(source_directory)
+    target_path = os.path.abspath(target_directory) 
+    
+    if os.path.exists(target_path) == True:
+        shutil.rmtree(target_directory) 
+        
+    os.mkdir(target_path)
+    
+    for file in os.listdir(source_path):
+        source_file = os.path.join(source_path, file)
+        target_file = os.path.join(target_path, file)
+        
+        if os.path.isfile(source_file) == True:
+            shutil.copy(source_file, target_file)
+        elif os.path.isdir(source_file) == True:
+            os.mkdir(target_file)
+            copy_contents(source_file, target_file)
+
+
+def copy_contents_prime(source_directory, target_directory): 
+    
+    source_path = os.path.abspath(source_directory)
+    target_path = os.path.abspath(target_directory) 
+    
+    if os.path.exists(target_path) == True:
+        shutil.rmtree(target_directory) 
+    
+    shutil.copytree(source_path, target_path) 
+
+
+def main():
+    copy_contents("./static/", "./public/")
     
 if __name__ == "__main__":
     main()
